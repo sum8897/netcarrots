cc39b8d2ad8b10c826de9d5f2a3c53e0224a156a
ashish <ashishchaurasiya610@gmail.com>
2022-09-08 20:10:26 +0530

    added loader in user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,53 +10,56 @@ import { UserService } from '../services/user.service';
 })
 export class LoginComponent implements OnInit {
 
-  
-  constructor(public auth:AuthService,
-             public router: Router,
-            public user: UserService) {}
-  ngOnInit() {
-  
-  }
-  loginRes:any;
-  loginData:any;
-  loginallData:any;
-  captchaCode:any;
-  otp:any;
-  oldOTP="123456";
- 
-  submitData(){
-    let user_data={
-      "MobileNo" : "9900000000",
-      "DeviceId" : "Device001",
-      "OSType" : "iOS",      
-      "CaptchaCode" : "",     
-      "OTP" : "",     
-      "IsLoginFirst" : "",    
-      "FCMToken" : ""
-    }
-    
- this.auth.loginUser(user_data).subscribe((data)=>{
-   this.loginRes=data;
-   this.loginData=this.loginRes.UserLoginAPI;
-   this.loginallData=this.loginData.Response;
-   this.captchaCode=this.loginallData[0].CaptchaCode;
-   this.otp=this.loginallData[0].OTPCode;
-   localStorage.setItem('otp',this.otp);
-   localStorage.setItem('allData',JSON.stringify(this.loginallData))
-   localStorage.setItem('result',this.loginData.Result);
-console.log(this.captchaCode+" "+this.otp);
 
-if(this.otp==this.oldOTP){
-this.router.navigateByUrl('/home');
-}
-else{
-  alert('OTP did not match..')
-}
- },err=>{
-   console.log(err);
- })
+  constructor(public auth: AuthService,
+    public router: Router,
+    public user: UserService) { }
+  ngOnInit() {
+
   }
-  submitDataInput(loginForm:any){
-  console.log(loginForm.value.phone);
+  loginRes: any;
+  loginData: any;
+  loginallData: any;
+  captchaCode: any;
+  otp: any;
+  oldOTP = "123456";
+
+  submitData() {
+    let user_data = {
+      "MobileNo": "9900000000",
+      "DeviceId": "Device001",
+      "OSType": "iOS",
+      "CaptchaCode": "",
+      "OTP": "",
+      "IsLoginFirst": "",
+      "FCMToken": ""
+    }
+    this.user.present('wait...');
+    this.auth.loginUser(user_data).subscribe((data) => {
+      this.user.dismiss();
+      this.loginRes = data;
+      this.loginData = this.loginRes.UserLoginAPI;
+      this.loginallData = this.loginData.Response;
+      this.captchaCode = this.loginallData[0].CaptchaCode;
+      this.otp = this.loginallData[0].OTPCode;
+      localStorage.setItem('otp', this.otp);
+      localStorage.setItem('allData', JSON.stringify(this.loginallData))
+      localStorage.setItem('result', this.loginData.Result);
+      console.log(this.captchaCode + " " + this.otp);
+
+      if (this.otp == this.oldOTP) {
+        this.router.navigateByUrl('/home');
+      }
+      else {
+        alert('OTP did not match..')
+      }
+    }, err => {
+      this.user.dismiss();
+      alert(JSON.stringify(err));
+    })
+  }
+
+  submitDataInput(loginForm: any) {
+    console.log(loginForm.value.phone);
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 loginAllData:any;
-  constructor() { }
+  constructor(public loadingController: LoadingController,) { }
+
+  
+  isLoading = false;
+           
+  async present( msg:any ) {
+    this.isLoading = true;
+    return await this.loadingController.create({
+     message: msg,
+     mode:'ios'
+    }).then(a => {
+      a.present().then(() => {
+        if (!this.isLoading) {
+          a.dismiss().then(() => console.log('abort presenting'));
+        }
+      });
+    });
+  }          
+  async dismiss() {
+    this.isLoading = false;
+    return await this.loadingController.dismiss().then(() => console.log('dismissed'));
+  }
+
 }
